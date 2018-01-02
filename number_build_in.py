8b18954269cc391b1a5412f4db42d9345bffbb77
KripaSharma2016 <kripashankarsharma95@gmail.com>
2018-01-02 21:58:32 +0530

    number and string build in added

---
+++ number_build_in.py
@@ -0,0 +1,66 @@
+'''
+Created on Jan 2, 2018
+
+@author: kripa
+'''
+"""print("hi")
+
+print(8+9)
+print(90+65)
+
+print("step-a")
+#num = 90
+#print(num)
+'''
+n_1_um = 80
+print("step_b")
+print(n_1_um)
+'''
+print("hi")
+"""
+#num_one = 90
+num_one = 9
+num_two = 8
+print(type(num_one))
+
+num_three = num_one % num_two
+print(num_three)
+print(type(num_three))
+
+hour =48
+minute = 60
+sec = 60
+print(hour*minute*sec)
+
+
+import  math
+print(math.pi)
+#print(math.sqrt(-4))
+
+import cmath
+print(cmath.sqrt(-4))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ string_build_in.py
@@ -0,0 +1,45 @@
+'''
+Created on Jan 2, 2018
+
+@author: kripa
+'''
+name = "rohan"
+address = 'blr'
+print(name)
+print(address)
+para = "let's play !"
+print(para)
+print("{}'s address is {}".format(name,address))
+print(name+"'s "+"address is "+address)
+
+details = name + address
+print(details)
+f_name = "prasad"
+l_name = "sharma"
+details = f_name +" "+ l_name
+print(details)
+#print(f_name[6])
+var = len(f_name)
+print(f_name[var-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
