883150b7fa984c1efab2a365ba9f3b2e911b50f7
KripaSharma2016 <kripashankarsharma95@gmail.com>
2018-01-04 07:45:46 +0530

    string build in

--- string_build_in.py
+++ string_build_in.py
@@ -22,6 +22,69 @@ print(details)
 var = len(f_name)
 print(f_name[var-1])
 
+#slicing
+""""
+print(f_name[1:3])
+print(f_name[:3])
+print("***************")
+#new_var = f_name[-2:2]
+#new_var = f_name[4:2] # pra
+#new_var = f_name[5:1] #pra
+new_var = f_name[-5:-3] # pra
+#new_var = f_name[3:0] # pra
+print(">>>>>>>>>>>>>")
+#new_var = f_name[-2:2]
+print(new_var)
+print("###############")
+print(type(new_var))
+if new_var == "":
+    print("hi")
+else:
+    print("bye")
+"""
+
+# predefined function for string
+
+para = "this is a boy."
+print(para.capitalize())
+print(para.count('boy'))
+print(para.split(' '))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
